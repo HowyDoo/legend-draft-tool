ce71729248f2356ec93a20488f90f6d6ba53a4cb
HowyDoo <howard.shum@yahoo.com>
2022-08-28 02:40:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const legend = document.querySelectorAll('.legend')
+const selections = document.querySelectorAll('.selections')
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+const div = document.querySelector('div')
+div.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+div.addEventListener('drop', (e) => {
+  e.preventDefault()
+})
+
+Lpick = ''
+
+legend.forEach(leg => {
+  leg.addEventListener('mousedown', () => {
+
+    legend.forEach(leg => {
+      leg.classList.remove('highlight')
+    })
+    leg.classList.add('highlight');
+
+    Lpick = leg.getAttribute('src');
+
+  })
+
+  leg.addEventListener('contextmenu', () => {
+    leg.classList.toggle('banned');
+  })
+
+})
+
+selections.forEach(select => {
+
+  select.addEventListener('click', () => {
+    select.innerHTML = `<img src="${Lpick}" class="imgcont" alt="Image">`
+  })
+
+  select.addEventListener('contextmenu', () => {
+    select.innerHTML = '';
+  })
+
+})
+
